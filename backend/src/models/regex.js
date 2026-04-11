f0f72512982d6898ef7d639bbacd01e5d5439a4f
Saika105 <saikamkhan367@gmail.com>
2026-04-11 19:50:06 +0600

    time conflict solved

--- backend/src/models/regex.js
+++ backend/src/models/regex.js
@@ -1,3 +1,3 @@
 export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export const PHONE_REGEX = /^(?:\+8801|8801|01)[3-9]\d{8}$/;
-export const TIME_REGEX = /^([01]\d|2[0-3]):([0-5]\d)$/;
+export const TIME_REGEX = /^(?:0?[1-9]|1[0-2]):[0-5]\d\s?(?:[AP]M|[ap]m)$|^([01]\d|2[0-3]):([0-5]\d)$/;
