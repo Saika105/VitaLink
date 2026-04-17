71f8d83f8b87a323a6cb57bc479c59e5dbabae69
Saika105 <saikamkhan367@gmail.com>
2026-04-17 22:21:57 +0600

    receiptionist part pata

--- backend/src/routes/receptionist.routes.js
+++ backend/src/routes/receptionist.routes.js
@@ -1,17 +1,26 @@
 import { Router } from "express";
 import { 
     loginReceptionist, 
-    logoutReceptionist 
+    logoutReceptionist, 
+    findPatientByUpid, 
+    searchTestsForBilling, 
+    createTestInvoice,
 } from "../controllers/receptionist.controller.js";
-import { upload } from "../middlewares/multer.middleware.js";
-import { verifyJWT , isReceptionist} from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { isReceptionist } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// Public
 router.route("/login").post(loginReceptionist);
 
 // Secured Routes
-router.route("/logout").post(verifyJWT, isReceptionist, logoutReceptionist);
+router.use(verifyJWT, isReceptionist); 
+
+router.route("/logout").post(logoutReceptionist);
+router.route("/search-patient").get(findPatientByUpid);
+router.route("/search-tests").get(searchTestsForBilling);
+router.route("/create-invoice").post(createTestInvoice);
 
 
 export default router;
